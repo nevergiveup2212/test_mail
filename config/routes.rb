ea0637eb38d6fb2a6b4b4a4deb21ab1b608a9c0e
nevergiveup2212 <blackcat22121996@gmail.com>
2018-01-22 14:49:44 +0700

    save data

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 	get 'index/welcome'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # 
+   post '/create', to: 'users#create' 
 end
